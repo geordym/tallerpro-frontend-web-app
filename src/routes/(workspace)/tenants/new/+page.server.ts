07954fe627f9d60cd8d8f5323291bdf5ad1b3353
GeordyM <ggeordymm@gmail.com>
2026-03-22 22:29:34 -0500

    añadidos acmbios

--- src/routes/(workspace)/tenants/new/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async ({ platform }) => {
-  return {
-    apiUrl: platform?.env?.PUBLIC_API_BACKEND_URL
-  };
-};
